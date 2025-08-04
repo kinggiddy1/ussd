4c95eb029b39d4474901eb1c3067c57d604d8616
kinggiddy1 <kinggiddy@gmail.com>
2025-08-04 21:37:58 +0200

    USSD Project

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/ussd', (req, res) => {
+    res.send('USSD endpoint is working.');
+  });
+  
+
+app.post('/ussd', (req, res) => {
+    let { sessionId, serviceCode, phoneNumber, text } = req.body;
+
+    let response = '';
+
+    let textValue = text.split('*');
+    switch (textValue[0]) {
+        case '':
+            response = `CON Welcome to MyService
+1. Check Balance
+2. Buy Airtime`;
+            break;
+        case '1':
+            response = `END Your balance is RWF 500`;
+            break;
+        case '2':
+            response = `END You have bought airtime successfully`;
+            break;
+        default:
+            response = `END Invalid choice`;
+    }
+
+    res.set('Content-Type', 'text/plain');
+    res.send(response);
+});
+
+app.listen(3000, () => {
+    console.log('USSD app running on port 3000');
+});
